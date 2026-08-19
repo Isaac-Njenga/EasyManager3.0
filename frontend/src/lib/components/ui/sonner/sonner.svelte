8d17bfa5c4c2436a7ddfdc6e4ad538164dc2bcf7
IsaacNjenga <njengaisaac789@gmail.com>
2026-08-19 12:53:38 +0300

    - removed mode

--- frontend/src/lib/components/ui/sonner/sonner.svelte
+++ frontend/src/lib/components/ui/sonner/sonner.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { mode } from "mode-watcher";
+	// import { mode } from "mode-watcher";
 	import { Toaster as Sonner, type ToasterProps as SonnerProps } from "svelte-sonner";
 	import Loader2Icon from '@lucide/svelte/icons/loader-2';
 	import CircleCheckIcon from '@lucide/svelte/icons/circle-check';
@@ -11,7 +11,7 @@
 </script>
 
 <Sonner
-	theme={mode.current}
+	// theme={mode.current}
 	class="toaster group"
 	style="--normal-bg: var(--color-popover); --normal-text: var(--color-popover-foreground); --normal-border: var(--color-border);"
 	{...restProps}
