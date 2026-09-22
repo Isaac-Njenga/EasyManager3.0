ecfdba4e2e106e9d41a9d321b12b12f900b72774
Isaac Njenga <ayzzoh20@gmail.com>
2026-09-22 10:07:58 +0300

    - left out dist routes

--- backend/dist/modules/Cohere/cohere.routes.js
+++ backend/dist/modules/Cohere/cohere.routes.js
@@ -3,14 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.CohereModelRouter = CohereModelRouter;
 const express_1 = require("express");
 const cohere_controller_1 = require("./cohere.controller");
-// import { protectRoute } from "../../middleware/auth.middleware";
-// import { adminRoute } from "../../middleware/admin.middleware";
+const auth_middleware_1 = require("../../middleware/auth.middleware");
+const admin_middleware_1 = require("../../middleware/admin.middleware");
 function CohereModelRouter() {
     const router = (0, express_1.Router)();
-    router.post("/generate-description", 
-    // protectRoute,
-    // adminRoute,
-    cohere_controller_1.handleGenerateDescription);
+    router.post("/generate-description", auth_middleware_1.protectRoute, admin_middleware_1.adminRoute, cohere_controller_1.handleGenerateDescription);
     return router;
 }
 //# sourceMappingURL=cohere.routes.js.map
